b6e05fece01977fa77f2269a2f66d6bb496124c6
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-04-12 23:56:42 +0200

    Updated the `startup.lua` script

--- Source/Scripts/startup.lua
+++ Source/Scripts/startup.lua
@@ -1,7 +1,7 @@
 SetProfilerEnabled(false)
 
 if pcall(getfenv, 4) then
-    print("Running the startup script as a library.")
+    error("Running the startup script as a library.")
 else
     print("Running the startup script as a program.")
 end
