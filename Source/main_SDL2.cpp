3ab74f362a5ede830d5429546f1d70255e701da4
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-04-15 22:29:13 +0200

    Gave the game an icon on Linux

--- Source/main_SDL2.cpp
+++ Source/main_SDL2.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <SDL2/SDL.h>
 #include "sdl2.h"
+#include "Distribution/Icon.h"
 
 int sdl_loop(Game &tests)
 {
@@ -48,6 +49,33 @@ int sdl_loop(Game &tests)
     return 0;
 }
 
+void set_window_icon(SDL_Window *window) {
+  // these masks are needed to tell SDL_CreateRGBSurface(From)
+  // to assume the data it gets is byte-wise RGB(A) data
+  Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+  int shift = (gimp_image.bytes_per_pixel == 3) ? 8 : 0;
+  rmask = 0xff000000 >> shift;
+  gmask = 0x00ff0000 >> shift;
+  bmask = 0x0000ff00 >> shift;
+  amask = 0x000000ff >> shift;
+#else // little endian, like x86
+  rmask = 0x000000ff;
+  gmask = 0x0000ff00;
+  bmask = 0x00ff0000;
+  amask = (gimp_image.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+  SDL_Surface *icon = SDL_CreateRGBSurfaceFrom(
+      (void *)gimp_image.pixel_data, gimp_image.width, gimp_image.height,
+      gimp_image.bytes_per_pixel * 8,
+      gimp_image.bytes_per_pixel * gimp_image.width, rmask, gmask, bmask,
+      amask);
+
+  SDL_SetWindowIcon(window, icon);
+
+  SDL_FreeSurface(icon);
+}
+
 int main(int argc, char *argv[])
 {
     Game tests;
@@ -68,6 +96,8 @@ int main(int argc, char *argv[])
     if (!window) {
         throw sdl2::SDLError("Error creating window");
     }
+
+    set_window_icon(window.get());
 
     tests.SetWindow(window.get());
 
