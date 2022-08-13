b21d2b5a4b7e60d541a59f546a4886b344929040
Megumumpkin <megumumpkin@protonmail.ch>
2022-08-13 15:14:44 +0700

    Resolve Build Errors

--- Source/Building/Build.sh
+++ Source/Building/Build.sh
@@ -31,5 +31,3 @@ rm -f Build/startup.lua
 rm -f Build/editor.lua
 cp Source/Scripts/Root/startup.lua Build/startup.lua
 cp Source/Scripts/Root/editor.lua Build/editor.lua
-rm -r -f Build/Data
-cp -r Data/ Build/Data
