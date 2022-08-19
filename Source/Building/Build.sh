2b5e88e80b7f4629d10f1f1c4e6b30761f9a0116
Megumumpkin <megumumpkin@protonmail.ch>
2022-08-19 17:42:31 +0700

    Buildscript Fix (Initial)

--- Source/Building/Build.sh
+++ Source/Building/Build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+# Please run this script in root
+
 if [ ! -d '../../Build' ]; then
 	mkdir '../../Build'
 fi
@@ -9,15 +11,14 @@ if [ ! -d '../../Data/Shader' ]; then
 fi
 
 if [ ! -d '../../Library/WickedEngine/build' ]; then
-	cd '../../Library/WickedEngine/'
-	mkdir 'build'
+	mkdir -p '../../Library/WickedEngine/build'
 	cd '../../Library/WickedEngine/build'
 	cmake .. -DCMAKE_BUILD_TYPE=Release
 	make -j$(nproc)
+	cd '../../../'
 fi
 
-if [ -d '../../Library/WickedEngine/build' ]; then
-	cd ../../
+if [ -d 'Library/WickedEngine/build' ]; then
 	cmake -B Build -DWickedEngine_DIR=Library/WickedEngine/build/cmake .
 else
 	echo "Wicked Engine build directory not found."
@@ -26,7 +27,5 @@ fi
 
 cmake --build Build -j$(nproc)
 
-rm -f Build/startup.lua
-rm -f Build/editor.lua
-cp Source/Scripts/Root/startup.lua Build/startup.lua
-cp Source/Scripts/Root/editor.lua Build/editor.lua
+cp -f Source/Scripts/Root/startup.lua Build/startup.lua
+cp -f Source/Scripts/Root/editor.lua Build/editor.lua
